49e965a4fbf7c1dfc432ad491f4233a4a6142022
Javi <itsjavii1610@gmail.com>
2026-01-21 09:28:31 +0100

    estrucutra

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -6,14 +6,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
 
-        int contador;
+        int contador = 1;
+        boolean juego = true;
 
         System.out.println("Introduce tu nombre:");
         String nombre = sc.next();
         System.out.println(nombre + " has sido seleccionado para trabajar como guardia nocturno en Freddy Fazbear's Pizzeria." +
                 "Buena suerte!");
 
+        while (juego) {
+            if (contador == 1) {
 
+            } else if (contador == 2) {
 
+            } else if (contador == 3) {
+
+            } else if (contador == 4) {
+
+            } else {
+
+            }
+        }
     }
 }
