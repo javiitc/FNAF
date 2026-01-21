87369d2c3fbb094b9b69d3bd9c1b392617023f8d
Javi <itsjavii1610@gmail.com>
2026-01-21 09:34:10 +0100

    noches

--- Java/src/Noches.java
+++ Java/src/Noches.java
@@ -1,3 +1,39 @@
 public class Noches {
 
+    public static void noche1() {
+        System.out.println("========================");
+        System.out.println("        NOCHE 1         ");
+        System.out.println("========================");
+        System.out.println("");
+
+    }
+
+    public static void noche2() {
+        System.out.println("========================");
+        System.out.println("        NOCHE 2         ");
+        System.out.println("========================");
+        System.out.println("");
+
+    }
+
+    public static void noche3() {
+        System.out.println("========================");
+        System.out.println("        NOCHE 3         ");
+        System.out.println("========================");
+        System.out.println("");
+    }
+
+    public static void noche4 () {
+        System.out.println("========================");
+        System.out.println("        NOCHE 4         ");
+        System.out.println("========================");
+        System.out.println("");
+    }
+
+    public static void noche5 (){
+        System.out.println("========================");
+        System.out.println("        NOCHE 5         ");
+        System.out.println("========================");
+        System.out.println("");
+    }
 }
