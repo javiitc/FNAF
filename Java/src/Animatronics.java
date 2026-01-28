41c16d9567a77142003981333afa09950635f242
Denise Andrea Name Ramirez <dnameramirez@gmail.com>
2026-01-28 09:39:59 +0100

    death improved

--- Java/src/Animatronics.java
+++ Java/src/Animatronics.java
@@ -10,54 +10,59 @@ public class Animatronics {
     int choice = 0;
     boolean isAlive = true;
 
-    public void bonnieEvent() {
+    public boolean bonnieEvent() {
 
         int[] chanceDeath = {1, 0 ,0, 0};
         int deathRoll;
         int energy = noches.energia;
 
-        System.out.println("You check the cameras and notice Bonnie's getting closer.");
+        while (isAlive) {
 
-        System.out.println("1. Hide");
-        System.out.println("2. Close door (-10 power)");
-        System.out.println("3. Do nothing");
-        System.out.println("Choose what to do: ");
+            System.out.println("You check the cameras and notice Bonnie's getting closer.");
 
-        choice = sc.nextInt();
+            System.out.println("1. Hide");
+            System.out.println("2. Close door (-10 power)");
+            System.out.println("3. Do nothing");
+            System.out.println("Choose what to do: ");
 
-        switch (choice) {
-            case 1:
-                System.out.println("You hide behind the Freddy mask.");
-                deathRoll = chanceDeath[random.nextInt(3)];
+            choice = sc.nextInt();
 
-                if (deathRoll == 1) {
-                    System.out.println("It didn't bypass his system!");
-                    isAlive = false;
-                } else if (deathRoll == 0) {
-                    System.out.println("It worked!");
-                    isAlive = true;
-                }
-                break;
-            case 2:
-                System.out.println("You close the door, consuming 10 power.");
-                energy -= 10;
-                break;
-            case 3:
-                System.out.println("You stay still as he scans the room.");
-                deathRoll = chanceDeath[random.nextInt(3)];
+            switch (choice) {
+                case 1:
+                    System.out.println("You hide behind the Freddy mask.");
+                    deathRoll = chanceDeath[random.nextInt(3)];
 
-                if (deathRoll == 0) {
-                    System.out.println("You've been caught!");
-                    isAlive = false;
-                } else if (deathRoll == 1) {
-                    System.out.println("He doesn't seem to notice!");
-                    isAlive = true;
-                }
-                break;
-            default:
-                System.out.println("Invalid option!");
-                break;
+                    if (deathRoll == 1) {
+                        System.out.println("It didn't bypass his system!");
+                        isAlive = false;
+                    } else if (deathRoll == 0) {
+                        System.out.println("It worked!");
+                        isAlive = true;
+                    }
+                    break;
+                case 2:
+                    System.out.println("You close the door, consuming 10 power.");
+                    energy -= 10;
+                    break;
+                case 3:
+                    System.out.println("You stay still as he scans the room.");
+                    deathRoll = chanceDeath[random.nextInt(3)];
+
+                    if (deathRoll == 0) {
+                        System.out.println("You've been caught!");
+                        isAlive = false;
+                    } else if (deathRoll == 1) {
+                        System.out.println("He doesn't seem to notice!");
+                        isAlive = true;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid option!");
+                    break;
+            }
         }
+
+        return isAlive;
 
     }
 
